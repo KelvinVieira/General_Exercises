c8ebcbfea345cd7bb2fb7dd070db6da130e0519a
KelvinVieira <kelvinvieirabut2@gmail.com>
2024-11-19 19:51:29 -0300

    Update MySQLtarefas.sql
    Updated SQL

--- projTarefa/MySQLtarefas.sql
+++ projTarefa/MySQLtarefas.sql
@@ -12,7 +12,7 @@ CREATE TABLE tbl_tarefa(
 	tar_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     tar_descr VARCHAR(50) NOT NULL,
     tar_setor VARCHAR(30) NOT NULL,
-    tar_status VARCHAR(25) NOT NULL,
     tar_prioridade VARCHAR(15) NOT NULL,
+    tar_status VARCHAR(25) NOT NULL,
     tar_data DATE NOT NULL
 );
