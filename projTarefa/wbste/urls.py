0427326895f76b3e65293a633d4a08fd90fc5609
KelvinVieira <kelvinvieirabut2@gmail.com>
2024-11-19 12:51:05 -0300

    Update urls.py
    Updated wbste/urls

--- projTarefa/wbste/urls.py
+++ projTarefa/wbste/urls.py
@@ -1,22 +1,37 @@
-"""
-URL configuration for projTarefas project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from wbste import views
+from wbste.views import *
+
+app_name = 'wbste'
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # GET /
+    path('', views.index, name='index'),
+
+    # -------------------------------------
+    # GET /usuarios
+    path('usuarios/', views.HomeViewUsuario.as_view(), name="usuarios_index"),
+
+    # Path de consulta (lista) de dados
+    path('usuarios/lista', ListViewUsuario.as_view(), name='usuarios_listagem'),
+
+    # Path de alteração de dados
+    path('usuarios/<pk>', UpdateViewUsuario.as_view(), name='usuarios_alteracao'),  # ou, com o uso de <slug> ao invés de <id> (<slug> trataria os dados diferentemente, em comparação a <id>)
+
+    # Path para deleção de dados
+    path('usuarios/excluir/<pk>', DeleteViewUsuario.as_view(), name='usuarios_delecao'),
+
+    # Path para criação de dados
+    path('usuarios/cadastrar/', CreateViewUsuario.as_view(), name='usuarios_criacao'),
+
+    # ------------------------------------
+    path('tarefas/', views.HomeViewTarefa.as_view(), name="tarefas_index"),
+
+    path('tarefas/lista', ListViewTarefa.as_view(), name='tarefas_listagem'),
+
+    path('tarefas/<pk>', UpdateViewTarefa.as_view(), name='tarefas_alteracao'),
+
+    path('tarefas/excluir/<pk>', DeleteViewTarefa.as_view(), name='tarefas_delecao'),
+
+    path('tarefas/cadastrar/', CreateViewTarefa.as_view(), name='tarefas_criacao'),
 ]
